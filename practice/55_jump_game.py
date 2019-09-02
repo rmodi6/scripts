e2fe4d7b4b75e75b2f2c56edd55e3375bfb8bcca
Ruchit Modi <modi.ruchit6@gmail.com>
2019-09-01 22:42:13 -0400

    Create 55_jump_game.py

---
+++ practice/55_jump_game.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        reachableIndex = 0
+        destIndex = len(nums) - 1
+        i = 0
+        while i <= reachableIndex and reachableIndex < destIndex:
+            reachableIndex = max(reachableIndex, i + nums[i])
+            i += 1
+        if reachableIndex >= destIndex:
+            return True
+        return False
+        
