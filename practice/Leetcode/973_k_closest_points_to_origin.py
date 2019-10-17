cb91c9a4e6e5d4b49a1e7be3114eea4ddf5153bb
Ruchit Modi <modi.ruchit6@gmail.com>
2019-10-17 13:24:32 -0400

    Added solution to k closest points to origin

---
+++ practice/Leetcode/973_k_closest_points_to_origin.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/k-closest-points-to-origin
+
+class Solution:
+    def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
+        
+        def distanceToOrigin(point):
+            return (point[0]**2 + point[1]**2) ** 0.5
+        
+        distanceToPointMap = {}
+        
+        for point in points:
+            d = distanceToOrigin(point)
+            l = distanceToPointMap.get(d, [])
+            l.append(point)
+            distanceToPointMap[d] = l
+        
+        ans = []
+        for d in sorted(distanceToPointMap.keys()):
+            ans.extend(distanceToPointMap[d])
+            if len(ans) >= K:
+                break
+                
+        return ans
