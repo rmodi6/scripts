4d27044368eef21614a8031ed66723095f8c8626
Ruchit Modi <modi.ruchit6@gmail.com>
2019-11-19 10:24:39 -0500

    Added solution for group anagrams

---
+++ practice/Leetcode/49_group_anagrams.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/group-anagrams/ 
+
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hashmap = defaultdict(list)
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            hashmap[sorted_word].append(word)
+        return hashmap.values()
