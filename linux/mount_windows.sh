f6a493eee170ce6a47e178898d7cca9b4b87dc63
Ruchit Modi <modi.ruchit6@gmail.com>
2020-10-20 18:29:10 -0500

    Mount ntfs drive as well

--- linux/mount_windows.sh
+++ linux/mount_windows.sh
@@ -3,5 +3,8 @@
 set -e
 mkdir -p /media/bitlocker
 mkdir -p /media/mount
-sudo dislocker -V /dev/nvme0n1p3 -- /media/bitlocker
+sudo dislocker -V /dev/nvme1n1p3 -- /media/bitlocker -o nonempty
 sudo mount -o loop /media/bitlocker/dislocker-file /media/mount
+
+mkdir -p /media/ntfs
+sudo mount -t ntfs /dev/nvme0n1p2 /media/ntfs
