9bdbf7ee4903162165352e7c10f468e62ba04d2b
Ruchit Modi <modi.ruchit6@gmail.com>
2020-10-22 17:58:58 -0500

    October challenge 22

---
+++ practice/Leetcode/3504_minimum_depth_of_binary_tree.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/explore/challenge/card/october-leetcoding-challenge/562/week-4-october-22nd-october-28th/3504/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        if not root.left:
+            return self.minDepth(root.right) + 1
+        if not root.right:
+            return self.minDepth(root.left) + 1
+        return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
