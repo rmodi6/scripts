74e794037bbbcc746ef2db2790248a978df7d0e3
Ruchit Modi <modi.ruchit6@gmail.com>
2020-12-02 11:16:50 -0600

    December challenge 1

---
+++ practice/Leetcode/3551_maximum_depth_of_binary_tree.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/explore/challenge/card/december-leetcoding-challenge/569/week-1-december-1st-december-7th/3551/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        return 0 if not root else max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
