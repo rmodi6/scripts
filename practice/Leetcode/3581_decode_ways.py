b01cb7238ac8e6d0d62f66fe18a6b9ca430a2d1f
Ruchit Modi <modi.ruchit6@gmail.com>
2020-12-26 15:53:20 -0600

    December challenge 26

---
+++ practice/Leetcode/3581_decode_ways.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/explore/challenge/card/december-leetcoding-challenge/572/week-4-december-22nd-december-28th/3581/
+
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        if not s or s[0] == '0':
+            return 0
+        dp = [0] * (len(s)+1)
+        dp[0] = 1
+        dp[1] = 1
+        for i in range(2, len(s) + 1):
+            if s[i-1] != '0':
+                dp[i] += dp[i-1]
+            temp = int(s[i-2:i])
+            if temp > 9 and temp < 27:
+                dp[i] += dp[i-2]
+        return dp[-1]
