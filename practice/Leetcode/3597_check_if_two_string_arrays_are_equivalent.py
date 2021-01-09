d72dfaa38daa81975fae25f2c8534503c3bf9e7e
Ruchit Modi <modi.ruchit6@gmail.com>
2021-01-09 17:20:00 -0600

    January 2021 challenge 8

---
+++ practice/Leetcode/3597_check_if_two_string_arrays_are_equivalent.py
@@ -0,0 +1,5 @@
+# https://leetcode.com/explore/challenge/card/january-leetcoding-challenge-2021/580/week-2-january-8th-january-14th/3597/
+
+class Solution:
+    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+        return ''.join(word1) == ''.join(word2)
