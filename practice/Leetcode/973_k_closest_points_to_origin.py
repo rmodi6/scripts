201b9dabd4e409d3e76d5265df39c87bd3d83253
Ruchit Modi <modi.ruchit6@gmail.com>
2021-12-28 18:57:09 -0600

    Add one liner solution

--- practice/Leetcode/973_k_closest_points_to_origin.py
+++ practice/Leetcode/973_k_closest_points_to_origin.py
@@ -21,3 +21,9 @@ class Solution:
                 break
                 
         return ans
+
+# One liner
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        return sorted(points, key=lambda point: (point[0]**2 + point[1]**2)**0.5)[:k]
